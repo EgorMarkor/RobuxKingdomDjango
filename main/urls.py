23bb756f1e13299ca647dafd7ba60a28226c4985
Markor <86112299+EgorMarkor@users.noreply.github.com>
2025-07-01 05:42:40 +0400

    Third Commit

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, BonusView, AccountView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('bonus/', BonusView.as_view(), name='bonus'),
+    path('account/', AccountView.as_view(), name='account')
 ]
--- main/views.py
+++ main/views.py
@@ -3,3 +3,11 @@ from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
     template_name = "robux_head_pc/index.html"
+
+
+class BonusView(TemplateView):
+    template_name = "robux_bonus_pc/index.html"
+    
+    
+class AccountView(TemplateView):
+    template_name = "robux_account_pc/index.html"
